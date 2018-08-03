d1ce73bf6b7b2d370ea1c74114d74cdccc001ac7
DESKTOP-U1J5E63\Kyle <krohlfing94@gmail.com>
2018-08-02 22:33:30 -0500

    Added some intro text

--- photography/src/App.js
+++ photography/src/App.js
@@ -13,9 +13,17 @@ class App extends Component {
           <Navbar/>
         </div>
         <aside className="sidebar">
-          <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Praesent dictum semper ligula, vehicula tincidunt neque efficitur eget. Quisque ornare velit quam, id sollicitudin erat cursus non. Praesent eget sodales orci, vestibulum faucibus neque. Etiam at enim sed erat maximus ultricies non at nulla. Aliquam vel odio a orci condimentum congue quis id ante. Maecenas sollicitudin mauris eu hendrerit pharetra. Sed sed ornare tellus. Curabitur nec metus id nisi luctus commodo rutrum quis leo. Morbi dignissim magna sit amet purus convallis, ut consectetur ligula iaculis. Phasellus tempus semper diam, non suscipit felis. Proin et augue quis ipsum pellentesque consectetur ac eu orci. Nulla placerat eros non felis ornare venenatis. Donec eget tortor et velit imperdiet mattis. Cras id justo ut massa sollicitudin suscipit in a arcu.</p>
+          <p>
+            <strong>WEBSITE COMING SOON.</strong>
+          </p>
+          <p>
+            Here you will be able to view the incredible wildlife photos taken by me: Kyle Rohlfing. I hope I can inspire you and teach you more about the animals from around the world. I hope to bring a small piece of the wilderness into your home.
+          </p>
+          <br />
+          <p>
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Praesent dictum semper ligula, vehicula tincidunt neque efficitur eget. Quisque ornare velit quam, id sollicitudin erat cursus non. Praesent eget sodales orci, vestibulum faucibus neque. Etiam at enim sed erat maximus ultricies non at nulla. Aliquam vel odio a orci condimentum congue quis id ante. Maecenas sollicitudin mauris eu hendrerit pharetra. Sed sed ornare tellus. Curabitur nec metus id nisi luctus commodo rutrum quis leo. Morbi dignissim magna sit amet purus convallis, ut consectetur ligula iaculis. Phasellus tempus semper diam, non suscipit felis. Proin et augue quis ipsum pellentesque consectetur ac eu orci. Nulla placerat eros non felis ornare venenatis. Donec eget tortor et velit imperdiet mattis. Cras id justo ut massa sollicitudin suscipit in a arcu.
+          </p>
 
-          <p>Vivamus pharetra suscipit condimentum. Vestibulum velit ex, dictum sed vulputate at, faucibus vitae tellus. Sed consectetur et turpis et pretium. Quisque sed ipsum venenatis, congue risus id, viverra felis. Aenean dignissim libero quis molestie consequat. Donec eleifend tortor eu libero rutrum, in hendrerit ante porttitor. Vivamus volutpat congue interdum. Fusce arcu metus, gravida vitae tempor sit amet, consectetur eget leo. Morbi venenatis felis vitae erat luctus pharetra. Donec nec massa dolor. Aliquam elementum rhoncus diam a tempor.</p>
         </aside>
         <main className="content"></main>
       </div>
