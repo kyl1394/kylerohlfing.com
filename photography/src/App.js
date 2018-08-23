9bce3ff9f98a38b88bb3b7f3a372f0c8b099ce5a
DESKTOP-U1J5E63\Kyle <krohlfing94@gmail.com>
2018-08-22 22:31:36 -0500

    Code cleanup

--- photography/src/App.js
+++ photography/src/App.js
@@ -1,6 +1,8 @@
 import React, { Component } from 'react';
-import Navbar from './Navbar/Navbar.js'
-import App2 from './App2.js'
+import Navbar from './Navbar/Navbar.js';
+import Header from './Header/Header.js';
+import Sidebar from './Sidebar/Sidebar.js';
+import Gallery from './Gallery/Gallery.js';
 
 import logo from './logo.svg';
 import './App.css';
@@ -8,102 +10,12 @@ import './App.css';
 class App extends Component {
   render() {
     return (
-      <App2 />
-
-      // <div className="mainlayout">
-      //   <div className="navbar">
-      //     <Navbar/>
-      //   </div>
-      //   <main className="content">
-      //     <img src="/background.jpg" className="background" />
-      //   </main>
-      //   <span className="divider">&nbsp;</span>
-      //   <aside className="sidebar content-padding">
-      //     <p>
-      //       <strong>WEBSITE COMING SOON.</strong>
-      //     </p>
-      //     <p>
-      //       Here you will be able to view the incredible wildlife photos taken by me: Kyle Rohlfing. I hope I can inspire you and teach you more about the animals from around the world. I hope to bring a small piece of the wilderness into your home.
-      //     </p>
-      //     <br />
-      //     <p>
-      //       Lorem ipsum dolor sit amet, consectetur adipiscing elit. Praesent dictum semper ligula, vehicula tincidunt neque efficitur eget. Quisque ornare velit quam, id sollicitudin erat cursus non. Praesent eget sodales orci, vestibulum faucibus neque. Etiam at enim sed erat maximus ultricies non at nulla. Aliquam vel odio a orci condimentum congue quis id ante. Maecenas sollicitudin mauris eu hendrerit pharetra. Sed sed ornare tellus. Curabitur nec metus id nisi luctus commodo rutrum quis leo. Morbi dignissim magna sit amet purus convallis, ut consectetur ligula iaculis. Phasellus tempus semper diam, non suscipit felis. Proin et augue quis ipsum pellentesque consectetur ac eu orci. Nulla placerat eros non felis ornare venenatis. Donec eget tortor et velit imperdiet mattis. Cras id justo ut massa sollicitudin suscipit in a arcu.
-      //     </p>
-      //
-      //   </aside>
-      //   <div className="gallery content-padding">
-      //     <div className="mainText">
-      //       hmm
-      //       hmmhmm
-      //       hmmhmm
-      //       hmmhmm
-      //       hmmhmm
-      //       hmmhmm
-      //       hmmhmm
-      //       hmmhmm
-      //       hmmhmm
-      //       hmmhmm
-      //       hmmhmm
-      //       hmmhmm
-      //       hmmhmm
-      //       hmmhmm
-      //       hmmhmm
-      //       hmmhmm
-      //       hmmhmm
-      //       hmmhmm
-      //       hmmhmm
-      //       hmmhmm
-      //       hmmhmm
-      //       hmmhmm
-      //       hmmhmm
-      //       hmmhmm
-      //       hmmhmm
-      //       hmmhmm
-      //       hmmhmm
-      //       hmmhmm
-      //       hmmhmm
-      //       hmmhmm
-      //       hmmhmm
-      //       hmmhmm
-      //       hmmhmm
-      //       hmmhmm
-      //       hmmhmm
-      //       hmmhmm
-      //       hmmhmm
-      //       hmmhmm
-      //       hmmhmm
-      //       hmmhmm
-      //       hmmhmm
-      //       hmmhmm
-      //       hmmhmm
-      //       hmmhmm
-      //       hmmhmm
-      //       hmmhmm
-      //       hmmhmm
-      //       hmmhmm
-      //       hmmhmm
-      //       hmmhmm
-      //       hmmhmm
-      //       hmmhmm
-      //       hmmhmm
-      //       hmmhmm
-      //       hmmhmm
-      //       hmmhmm
-      //       hmmhmm
-      //       hmmhmm
-      //       hmmhmm
-      //       hmmhmm
-      //       hmmhmm
-      //       hmmhmm
-      //       hmmhmm
-      //       hmmhmm
-      //       hmmhmm
-      //       hmmhmm
-      //       hmmhmm
-      //       hmm
-      //     </div>
-      //   </div>
-      // </div>
+      <div className="pagelayout">
+        <Header className="header"/>
+        <Navbar className="navbar"/>
+        <Sidebar className="sidebar"/>
+        <Gallery className="gallery"/>
+      </div>
     );
   }
 }
